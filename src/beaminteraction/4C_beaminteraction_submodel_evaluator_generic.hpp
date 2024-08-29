a56715e54e0f3f368f67d9324269bfa229096679
Ivo Steinbrecher <ivo.steinbrecher@unibw.de>
2024-08-29 07:39:13 +0000

    Beaminteraction working with Lagrange multipliers

--- src/beaminteraction/4C_beaminteraction_submodel_evaluator_generic.hpp
+++ src/beaminteraction/4C_beaminteraction_submodel_evaluator_generic.hpp
@@ -258,6 +258,12 @@ namespace BeamInteraction
 
       virtual void assemble_stiff(Core::LinAlg::SparseOperator& jac){};
 
+      virtual Teuchos::RCP<Epetra_Map> get_lagrange_map() { return Teuchos::null; }
+
+      virtual void assemble_force(Epetra_Vector& f){};
+
+      virtual void assemble_stiff(Core::LinAlg::SparseOperator& jac){};
+
       //! @}
      protected:
       //! init flag
